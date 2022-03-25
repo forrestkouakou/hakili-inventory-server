51964a84bda352de3154898f9c59771cfbd60667
Forrest KOUAKOU <web.kforrest@gmail.com>
2022-03-25 18:48:16 +0000

    Unecessary change for heroku

--- hakili/conf/__init__.py
+++ hakili/conf/__init__.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from .drf import *
 from .email import *
 
-if settings.DEBUG:
-    from .logging import *
+# if settings.DEBUG:
+#     from .logging import *
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
